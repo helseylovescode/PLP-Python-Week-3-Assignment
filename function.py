2e0bf3fe90c6c8eb7eb47e732bc13b1eb87c0481
helseylovescode <oyujelsey@gmail.com>
2024-11-28 15:02:31 +0300

    Complete Assignment

---
+++ function.py
@@ -0,0 +1,24 @@
+def calculate_discount(price, discount_percent):
+  if discount_percent >= 20:
+     discount_amount = (price * discount_percent)/100
+     final_price = price - discount_amount
+     return final_price
+  else:
+     return price
+   
+   
+# Prompt the user for input
+try:
+    original_price = float(input("Enter the original price of the item: "))
+    discount_percentage = float(input("Enter the discount percentage: "))
+    
+    # Calculate the final price
+    final_price = calculate_discount(original_price, discount_percentage)
+    
+    # Print the result
+    if final_price < original_price:
+        print(f"The final price after applying the discount is: Ksh.{final_price:.2f}")
+    else:
+        print(f"No discount applied. The original price is: Ksh.{original_price:.2f}")
+except ValueError:
+    print("Invalid input. Please enter numerical values for price and discount percentage.")
